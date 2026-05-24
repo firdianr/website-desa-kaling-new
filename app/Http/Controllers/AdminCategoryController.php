289c4da7092bb9a71586e7308e501251fc9e5715
firdianr <firdianganteng001@gmail.com>
2026-05-24 11:37:17 +0700

    Memperbaiki menu admin kelola kategori berita

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\category;
+use App\Models\Category;
 use Database\Seeders\CategorySeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
